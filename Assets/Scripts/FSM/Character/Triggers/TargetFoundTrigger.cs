dd197dea70251c236e51ed23817c28250a728bb7
yoyoman <1955925300@qq.com>
2021-07-22 21:19:18 +0800

    调整敌人贴图

--- Assets/Scripts/FSM/Character/Triggers/TargetFoundTrigger.cs
+++ Assets/Scripts/FSM/Character/Triggers/TargetFoundTrigger.cs
@@ -1,14 +1,21 @@
-using UnityEngine;
 using EveryFunc;
-public class TargetFoundTrigger : FSMTrigger
-{
-    public override void Init()
-    {
+using UnityEngine;
+public class TargetFoundTrigger : FSMTrigger {
+    public override void Init () {
         triggerID = FSMTriggerID.TargetFound;
     }
-    public override bool HandleTrigger(FSMBase fsm)
-    {
-        return fsm.targetTF!=null;
+    public override bool HandleTrigger (FSMBase fsm) {
+        if (CheckTargetInLimit (fsm))
+            Debug.Log ("checktargetLimit:" + CheckTargetInLimit (fsm));
+        return fsm.targetTF != null && CheckTargetInLimit (fsm);
     }
-
+    public bool CheckTargetInLimit (FSMBase fsm) {
+        if (fsm.targetTF == null) return false;
+        //Debug.Log ("fsm:" + fsm.transform.position + ",leftdown:" + GridManager.Instance.LeftDownTF.position + ",right:" + GridManager.Instance.RightUpTF.position);
+        if (fsm.targetTF.transform.position.x > GridManager.Instance.RightUpTF.position.x ||
+            fsm.targetTF.transform.position.x < GridManager.Instance.LeftDownTF.position.x ||
+            fsm.targetTF.transform.position.y > GridManager.Instance.RightUpTF.position.y ||
+            fsm.targetTF.transform.position.y < GridManager.Instance.LeftDownTF.position.y) return false;
+        return true;
+    }
 }
