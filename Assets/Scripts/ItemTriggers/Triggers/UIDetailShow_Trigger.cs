86f9d2ca2181a82ff5a19df0b68806446043b915
yoyo <943898322@qq.com>
2021-07-27 11:44:13 +0800

    UI修改调效，修复字符突然出现问题

--- Assets/Scripts/ItemTriggers/Triggers/UIDetailShow_Trigger.cs
+++ Assets/Scripts/ItemTriggers/Triggers/UIDetailShow_Trigger.cs
@@ -17,7 +17,9 @@ public class UIDetailShow_Trigger : ITrigger
         //Debug.Log("UIDetailShow:名称：" + name + ",详细：" + detail);
         //TODO:不同情况下调用不同弹窗
         //UIManager.Instance.CallTalkUI(detail);
-        UIManager.Instance.CallDetailUI(name, detail_title,detail_index, objectImage,false);
+        string title = detail_index.Replace("<size=200><color=white>", "");
+        title = title.Replace("</color></size>", "");
+        UIManager.Instance.CallDetailUI(name, title, detail_index, objectImage, false);
 
     }
 }
